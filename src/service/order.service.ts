b32dcc47926d965d90cbaa1003f6ca52208c9bf2
Felipe Santaniello <felipe_sbc_sp@hotmail.com>
2023-08-17 08:39:34 -0300

    Alterado

--- src/service/order.service.ts
+++ src/service/order.service.ts
@@ -4,6 +4,15 @@ import OrderItem from "../entity/order_item";
 import Order from "../aggregate/order";
 
 
+/***
+ * Embora seja verdade que uma Domain Service em DDD não mantém estado de domínio, isso não implica que seus métodos devem ser obrigatoriamente estáticos. Na verdade, existem várias razões para se utilizar instâncias de Domain Services em vez de métodos estáticos. 
+ * Abaixo, segue algumas vantagens de se utilizar métodos de instância ao invés de métodos estáticos:
+ * - Injeção de Dependências;
+ * - Mais fácil de testar;
+ * - Polimorfismo;
+ * - Configuração de Estado Temporário (Embora um Domain Service não mantenha estado de domínio, em alguns casos, ele pode precisar de algum estado temporário para realizar sua operação. Por exemplo, ele pode precisar de algumas configurações que são injetadas nele no momento da criação e que são usadas somente durante a operação que ele realiza.)
+ */
+
 export default class OrderService {
   static placeOrder(customer: Customer, items: OrderItem[]): Order {
     if (items.length === 0) {
